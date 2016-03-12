b74481e032a98634c5855e5510a7600999577f5f
Deepak Tiwari <deepak.tiwari@webientsoft.com>
2016-03-12 23:26:52 +0530

    Thymeleaf integration

--- esykart-store/esykart-store-merchant/src/main/java/com/webientsoft/esykart/store/merchant/web/controller/DefaultController.java
+++ esykart-store/esykart-store-merchant/src/main/java/com/webientsoft/esykart/store/merchant/web/controller/DefaultController.java
@@ -1,13 +1,21 @@
 package com.webientsoft.esykart.store.merchant.web.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String home() {
 		return "index";
 	}
+	
+	@RequestMapping(value="/error",produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String error() {
+		return " exception occerod";
+	}
+	
 }
