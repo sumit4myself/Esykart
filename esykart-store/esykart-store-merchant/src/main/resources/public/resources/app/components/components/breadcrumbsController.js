150c1d5f6f652ca7982e112fecb7c41b4ecf3e2c
Sumit Kumar Sharma <sumit4myself@gmail.com>
2016-03-13 22:51:21 +0530

    resources moved to public

--- esykart-store/esykart-store-merchant/src/main/resources/public/resources/app/components/components/breadcrumbsController.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module('altairApp')
-    .controller('breadcrumbsCtrl', [
-        '$scope',
-        '$rootScope',
-        function ($scope,$rootScope) {
-            $rootScope.toBarActive = true;
-        }
-    ]);
