9ed390cea7774875514be753bdd9dc655f89a00d
Deepak Tiwari <deepak.tiwari@webientsoft.com>
2016-03-13 18:22:26 +0530

    Thymeleaf Integration

--- esykart-store/esykart-store-customer/src/main/java/com/webientsoft/esykart/customer/config/CustomerStoreUIApplication.java
+++ esykart-store/esykart-store-customer/src/main/java/com/webientsoft/esykart/customer/config/CustomerStoreUIApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
-@EnableHazelcastHttpSession
 @SpringBootApplication(scanBasePackages = { "com.webientsoft.esykart" })
 public class CustomerStoreUIApplication {
 
