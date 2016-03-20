edeff73f00e3451796810b1ffbb81467e937a818
jawahar lal gupta <antu.united@gmail.com>
2016-03-20 11:36:48 +0530

    Merchant Ui changes

--- esykart-store/esykart-store-merchant/src/main/resources/public/app/components/merchant/js/merchantController.js
+++ esykart-store/esykart-store-merchant/src/main/resources/public/app/components/merchant/js/merchantController.js
@@ -3,14 +3,25 @@ angular.module('altairApp')
 		'AddMerchantController',
 		[ '$scope', '$rootScope', 'utils', 'MerchantService',
 		function($scope, $rootScope, utils, MerchantService) {
+		    var $merchant_form = $("#merchant_form");
+			var parsleyForm = $merchant_form.parsley();
+			$scope.merchant = {};
+			$scope.validateMerchantDetails = function(context){
+				var result  = parsleyForm.validate({group: 'block1', force: true})
+				console.log("result = "+result);
+				return result;
+			}
+			
+			$scope.saveMerchant = function(){
+			   var form_serialized = JSON.stringify( utils.serializeObject($merchant_form), null, 2 );
+               // UIkit.modal.alert('<p>Wizard data:</p><pre>' + form_serialized + '</pre>');
+                //console.log($scope.merchant );
+                MerchantService._save(form_serialized);
+                return true;
+			}
+	} ])
 
-			
-			
-			
-			
-		} ])
-
-.controller(
+     .controller(
 		'ManageMerchantController',
 		[ '$scope', '$rootScope', 'utils', 'MerchantService',
 		function($scope, $rootScope, utils, MerchantService) {
--- esykart-store/esykart-store-merchant/src/main/resources/public/app/components/merchant/js/merchantService.js
+++ esykart-store/esykart-store-merchant/src/main/resources/public/app/components/merchant/js/merchantService.js
@@ -1,12 +1,41 @@
-﻿angular.module('altairApp').constant('url', 'rest/user/')
+﻿angular.module('altairApp').constant('url', 'rest/merchant/')
 .service('MerchantService',
 		['$http','utils','url',
 		function($http, utils, url) {
-	
-	
-	
-	
-	
-	
+			var service = {};
+	        service.save = _save;
+	        service.update = _update;
+	        service.updatePreference = _updatePreference;
+	        service.find = _find;
+	        service.search = _search;
+	        service.changeStatus = _changeStatus;
+	        return service;
+			
+//			private functions
+			function _save(merchant){
+				console.log('Merchant Service called');
+				return $http.post(url,merchant)
+							.then(utils.handleSuccess, utils.handleError('Error occurred while saving user.'));
+			}
+			function _update(merchantId,merchant){
+				return $http.put(url+"/" +merchantId ,merchant)
+							.then(utils.handleSuccess, utils.handleError('Error occurred while updating user.'));
+			}
+			function _updatePreference(merchantId,preference) {
+	            return $http.put(url,preference)
+	            			.then(utils.handleSuccess, utils.handleError('Error occurred while updaing user preferance.'));
+	        }
+			function _changeStatus(ids,status){
+				/*return $http.delete(url +"/" + status )
+							.then(utils.handleSuccess, utils.handleError('Somting went wrong please retry.'));*/
+			}
+			function _find(id){
+				return $http.get(url +"/" +id)
+							.then(utils.handleSuccess, utils.handleError('Error occurred while finding user.'));
+			}
+			function _search(filter){
+				return $http.get(url +"/search",filter)
+							.then(utils.handleSuccess, utils.handleError('Error occurred while searching user.'));
+			}
 		} 
 ]);
